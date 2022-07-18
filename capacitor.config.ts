5bd9ba1e91f62678c7c23d0a69dab14af987149f
VidMM <102604076+MrVidMM@users.noreply.github.com>
2022-07-18 11:56:52 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Restaurante_don_jose',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
